a626b3f20d80fef77350f42244442ca5762a7c0d
Gustav Grund Pihlgren <guspih@github.com>
2024-05-14 12:05:53 +0200

    docs: Added comments explaining image_visualizers

--- image_visualizers.py
+++ image_visualizers.py
@@ -16,6 +16,18 @@ from image_perturbers import SingleColorPerturber, ReplaceImagePerturber, cast_i
 from image_segmenters import perturbation_masks
 
 class TopVisualizer():
+    '''
+    Creates a numpy image showing the top attributed segments by perturbing the
+    lower attributed segments. How many top segments to display can be set as
+    a specific nr (k), fraction of total attribution (p), percentage of
+    segments (percent), or segments with attribution above a given treshold.
+    Args:
+        k (int): Nr of top segments to display
+        p (float): Display segments with total attribution at least p
+        percent (float): The fraction of top segments to display
+        treshold (float): The treshold above which to display segments
+        perturber (callable): Used to perturb lower attributed segments
+    '''
     def __init__(
         self, k=None, p=None, percent=None, treshold=None, perturber=None
     ):
@@ -40,6 +52,14 @@ class TopVisualizer():
             self.mode = 'treshold'
 
     def __call__(self, values, image, segment_masks, pixel_mask=None):
+        '''
+        Args:
+            values (array): [S] array of attribution scores per segment
+            image (array): Image to visualize attribution for of shape [H,W,C]
+            segment_masks (array): [S,H,W] array of 0/1 masks for the segments
+            pixel_mask (array): [H,W] array of attributions per pixel (unused)
+        Returns (array): [H,W,C] array visualizing the attribution on the image
+        '''
         nr_segments = values.shape[0]
         to_perturb = np.ones((1,nr_segments))
         if self.mode in ['percent', 'k']:
@@ -60,13 +80,20 @@ class TopVisualizer():
                 if total_value > self.p:
                     break
             to_perturb[0, top_order[:-(i+1)]] = 0
-
-
         distortion_mask = perturbation_masks(segment_masks, to_perturb)
         perturbed_image = self.perturber(image, distortion_mask, to_perturb)[0]
         return perturbed_image[0]
 
 class HeatmapVisualizer():
+    '''
+    Creates a numpy images showing the attribution of the image by creating a
+    heatmap overlaid on top of the original image.
+    Args:
+        normalize (bool): Whether to normalize attributions in range [0,1]
+        image_weight (float): How visible should the image be under the heatmap
+        colormap (cv2.ColormapTypes): The OpenCV colormap that the heatmap uses
+        invert_colormap (bool): Whether to swap the direction of the colormap
+    '''
     def __init__(
             self, normalize=False, image_weight=0.5, colormap=cv2.COLORMAP_JET,
             invert_colormap=False
@@ -78,6 +105,14 @@ class HeatmapVisualizer():
         self.invert_colormap = invert_colormap
 
     def __call__(self, values, image, segment_masks, pixel_mask=None):
+        '''
+        Args:
+            values (array): [S] array of attribution scores per segment
+            image (array): Image to visualize attribution for of shape [H,W,C]
+            segment_masks (array): [S,H,W] array of 0/1 masks for the segments
+            pixel_mask (array): [H,W] array of attributions per pixel (unused)
+        Returns (array): [H,W,C] array visualizing the attribution on the image
+        '''
         if pixel_mask is None:
             heatmap = perturbation_masks(segment_masks, values.reshape((1,-1)))
         else:
