980f0867de568e8cdb6eeefb8280c1c67b44a2d9
Gustav Grund Pihlgren <guspih@github.com>
2024-05-16 10:23:39 +0200

    feat: improved AUC evaluation and class for full image AUC evaluation

--- evaluation.py
+++ evaluation.py
@@ -11,7 +11,11 @@ from scipy.ndimage import gaussian_filter
 from PIL import Image
 import numbers
 
-def auc_sampler(vals, sample_size=None,  deletion=False):
+from image_perturbers import SingleColorPerturber, ReplaceImagePerturber, cast_image
+from image_segmenters import perturbation_masks
+
+
+def auc_sampler(vals, sample_size=None, deletion=False):
     '''
     Creates an array of samples where each following sample has more inserted
     or deleted samples than the preceeding one. The most influential features,
@@ -23,11 +27,11 @@ def auc_sampler(vals, sample_size=None,  deletion=False):
     Returns (array): [sample_size, M] array indicating the features to perturb
     '''
     if sample_size is None:
-        sample_size = vals.size
-    elif sample_size > vals.size:
+        sample_size = vals.size+1
+    elif sample_size > vals.size+1:
         raise ValueError(
-            f'sample_size must be <= vals.size, but sample_size={sample_size} '
-            f'and vals.size={vals.size}.'
+            f'sample_size must be <= vals.size+1, but '
+            f'sample_size={sample_size} and vals.size={vals.size}.'
         )
     indices = np.argsort(vals)[::-1]
     point = 1
@@ -37,13 +41,44 @@ def auc_sampler(vals, sample_size=None,  deletion=False):
     else:
         point = 1
         samples = np.zeros((sample_size, vals.size))
-    step = vals.size/sample_size
-    i=0
     old_j=0
-    for n in range(sample_size):
-        i+=step
+    for n, i in enumerate(np.linspace(0,vals.size,sample_size)):
         j = round(i)
         idxs = indices[old_j:j]
         samples[n:, idxs] = point
         old_j = j
     return samples
+
+class ImageAUCEvaluator():
+    def __init__(self, mode='srg', perturber=None, return_curves=False):
+        self.delete = []
+        if mode != 'deletion':
+            self.delete.append(False)
+        if mode != 'insertion':
+            self.delete.append(True)
+        self.mode = mode
+        self.perturber = perturber
+        self.return_curves = return_curves
+        if perturber is None:
+            self.perturber = SingleColorPerturber((190,190,190))
+
+    def __call__(self, image, masks, model, vals, output_idxs=..., **kwargs):
+        scores = []
+        curves = []
+        for delete in self.delete:
+            samples = auc_sampler(vals, kwargs.get('sample_size'), delete)
+            distortion_masks = perturbation_masks(masks, samples)
+            perturbed_images, perturbed_samples = self.perturber(
+                image, distortion_masks, samples
+            )
+            ys = model(perturbed_images)
+            if len(ys.shape)==1:
+                ys = np.expand_dims(ys, axis=-1)
+            curves.append(ys[output_idxs])
+            scores.append(np.mean(ys, axis=0)[output_idxs])
+        if self.mode == 'srg':
+            scores.append(scores[0]-scores[1])
+        if self.return_curves:
+            return scores, curves
+        return scores
+
