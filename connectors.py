6f0d46a8630c619f69bcb83287b6f275112cec05
Gustav Grund Pihlgren <guspih@github.com>
2024-05-17 11:57:06 +0200

    docs: Commented SegmentationAttributionPipeline

--- connectors.py
+++ connectors.py
@@ -4,6 +4,18 @@ from image_segmenters import perturbation_masks
 
 
 class SegmentationAttribuitionPipeline():
+    '''
+    Creates an image attribution pipeline that automatically performs and
+    connects segmentation, sampling, perturbing, model prediction, and 
+    attribution.
+    Args:
+        segmenter (callable): Return [H,W], [S,H,W] segments and S masks
+        sampler (callable): Return [N,S] N samples of segments to perturb
+        perturber (callable): Return [N,H,W,C], [N,S] perturbed images, samples
+        explainer (callable): Calculates attribution based on perturbation
+        mask_transform (callable): Return [S,H,W] transformed masks
+        per_pixel (bool): Whether to return [O,H,W] the attribution per pixel
+    '''
     def __init__(
         self, segmenter, sampler, perturber, explainer, mask_transform=None,
         per_pixel=False
@@ -21,15 +33,20 @@ class SegmentationAttribuitionPipeline():
         self.samples = None
         self.ys = None
     
-    def __call__(self, image, model, **kwargs):
+    def __call__(self, image, model, sample_size=None):
+        '''
+        Args:
+            image (array): the [H,W,C] image to explain via attribution
+            model (callable): The prediction model returning [N,O] for output O
+            sample_size (int): The nr N of perturbed images to use to attribute
+        Returns: Output of explainer for each output O, or [O,H,W] if per_pixel
+        '''
         segments, self.masks = self.segmenter(image)
         if not self.mask_transform is None:
             self.transformed_masks = self.mask_transform(self.masks)
         else:
             self.transformed_masks = self.masks
-        self.samples = self.sampler(
-            self.masks.shape[0], sample_size=kwargs.get('sample_size')
-        )
+        self.samples = self.sampler(self.masks.shape[0], sample_size)
         distortion_masks = perturbation_masks(
             self.transformed_masks, self.samples
         )
