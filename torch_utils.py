df64c4bad0269d66d17edb81f3c8979edac71433
Gustav Grund Pihlgren <guspih@github.com>
2024-06-13 14:54:46 +0200

    feat: TorchModelWrapper can now be made to add softmax to the output with softmax_out argument

--- torch_utils.py
+++ torch_utils.py
@@ -5,26 +5,35 @@ import torch.nn as nn
 
 class TorchModelWrapper(nn.Module):
     def __init__(
-        self, model, input_transforms, output_idxs=..., contrast=None,
-        gpu=False
+        self, model, input_transforms, softmax_out=False, output_idxs=...,
+        contrast=None, gpu=False
     ):
         super().__init__()
         self.model = model
         self.input_transforms = input_transforms
+        self.softmax = None
+        if softmax_out:
+            self.softmax = nn.Softmax(dim=1)
         self.output_idxs = output_idxs
         self.contrast = contrast
         self.gpu = gpu
+        
         if gpu:
             model.cuda()
 
     def __str__(self):
-        return self.model.__class__.__name__
+        content = f'{self.model.__class__.__name__}'
+        if not self.softmax is None: content += f',Softmax()'
+        return f'TorchModelWrapper({content})'
 
     def __call__(self, x):
         x = self.input_transforms(x)
         if self.gpu:
             x = x.cuda()
-        y = self.model(x).numpy(force=True)[:, self.output_idxs]
+        y = self.model(x)
+        if not self.softmax is None:
+            y = self.softmax(y)
+        y = y.numpy(force=True)[:, self.output_idxs]
         if not self.contrast is None:
             positive = np.sum(y[:,self.contrast[0]], axis=1)
             y = positive - np.sum(y[:,self.contrast[1]], axis=1)
