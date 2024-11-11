07feab3b5560950bb7cda124c150aaff01fc3e1e
Gustav Grund Pihlgren <guspih@github.com>
2024-11-11 15:07:26 +0100

    feat: Added shift_perturbation_masks() to shift masks as is done in the original RISE implementation

--- post_hoc/image_segmenters.py
+++ post_hoc/image_segmenters.py
@@ -159,3 +159,30 @@ def perturbation_masks(segment_masks, samples):
     '''
     distortion_masks = np.tensordot(samples, segment_masks, axes=(1,0))
     return distortion_masks/np.sum(segment_masks, axis=0)
+
+
+# Perturbation mask manipulators
+def shift_perturbation_masks(masks, shift, random=False):
+    '''
+    Shifts the perturbation masks horizontally and vertically by the given
+    amounts for each mask. The given amount can be specified per mask or for all
+    masks. If random is True then the shift for each mask is uniformly random
+    between 0 and the given amount.
+
+    Args:
+        masks (array): [N,H,W] array of masks in [0,1] (lower=more perturbation)
+        shift (array): [N,2] or [2] array of masks containing shift amounts
+        random (bool): Whether shift should be randomly drawn from [0,shift)
+    '''
+    if random:
+        shift = (np.random.rand(*shift.shape)*shift).astype(int)
+    if len(shift.shape) == 2:
+        if shift.shape[0] != masks.shape[0]:
+            raise ValueError('size of shift must equal masks in dim 0')
+        for i, shf in enumerate(shift):
+            masks[i] = np.roll(masks[i], shf, axis=(1,0))
+        return masks
+    elif len(shift.shape) == 1:
+        return np.roll(masks, shift, axis=(2,1))
+    else:
+        raise ValueError('shift must have shape [N,2] or [2]')
