cbf7f579324b646d402dacfe1e86b0daba2b0209
Gustav Grund Pihlgren <guspih@github.com>
2024-11-13 09:03:55 +0100

    feat: Implemented LIME baseline equivalent to the default image explainer in the lime package

--- post_hoc/baselines.py
+++ post_hoc/baselines.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 from .image_segmenters import (
-    perturbation_masks, shift_perturbation_masks, GridSegmenter
+    perturbation_masks, shift_perturbation_masks, GridSegmenter,
+    WrapperSegmenter
 )
 from .samplers import UniqueRandomSampler
-from .image_perturbers import SingleColorPerturber
-from .explainers import RISEAttributer
+from .image_perturbers import SingleColorPerturber, SegmentColorPerturber
+from .explainers import RISEAttributer, LinearLIMEAttributer
+from .connectors import SegmentationAttribuitionPipeline
 
 '''
 This file contains implementations equivalent or analogous to the original or
@@ -27,9 +29,9 @@ class RISEPipeline():
         batch_size (int): How many perturbed images to feed the model at once
     '''
     def __init__(
-            self, segmenter=None, sampler=None, perturber=None,
-            pad_image=(1/7,1/7), pad_mode='reflect', batch_size=None
-        ):
+        self, segmenter=None, sampler=None, perturber=None, pad_image=(1/7,1/7),
+        pad_mode='reflect', batch_size=None
+    ):
         self.segmenter = segmenter
         if segmenter is None:
             self.segmenter = GridSegmenter(7, 7, True)
@@ -116,3 +118,33 @@ class RISEPipeline():
             ys = np.expand_dims(self.ys, axis=-1)
         self.ys = ys
         return [self.explainer(y, distortion_masks) for y in ys.T]
+
+def LIMEPipeline(
+    segmenter=None, sampler=None, perturber=None, explainer=None,
+    per_pixel=False, batch_size=None
+):
+    '''
+    An implementation of LIME for images based on the default implementation in
+    the lime python package. By default this gives the original implementation
+    but some parameters may be altered.
+
+    Args:
+        segmenter (callable): Returns [H,W], [S,H,W] segments and S masks
+        sampler (callable): Returns [N,S] N samples of segments to perturb
+        perturber (callable): Returns [M,H,W,C], [M,S] perturbed images, samples
+        batch_size (int): How many perturbed images to feed the model at once
+    '''
+    if segmenter is None:
+        from skimage.segmentation import quickshift
+        segmenter = WrapperSegmenter(
+            quickshift, kernel_size=4, max_dist=200, ratio=0.2
+        )
+    if sampler is None:
+        sampler = UniqueRandomSampler()
+    if perturber is None:
+        perturber = SegmentColorPerturber(mode='mean')
+    if explainer is None:
+        explainer = LinearLIMEAttributer()
+    return SegmentationAttribuitionPipeline(
+        segmenter, sampler, perturber, explainer, per_pixel, batch_size=None
+    )
